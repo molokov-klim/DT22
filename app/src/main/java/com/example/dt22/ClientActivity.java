88a032afa5463fe59f304e87120315af9cce09dd
molokov-klim <ultrakawaii9654449192@gmail.com>
2022-06-08 20:04:12 +0300

    client sending work

--- app/src/main/java/com/example/dt22/ClientActivity.java
+++ app/src/main/java/com/example/dt22/ClientActivity.java
@@ -2,6 +2,7 @@ package com.example.dt22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,11 +12,14 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.PrintWriter;
 
 public class ClientActivity extends AppCompatActivity {
 
     EditText token;
     String ip = "188.127.239.39";
+    String messageToServer, messageFromServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +30,33 @@ public class ClientActivity extends AppCompatActivity {
 
     public void connectSession(View v) {
         BackgroundTask b = new BackgroundTask();
-        b.execute(ip, "connectSession "+token.getText().toString());
+        messageToServer = "connectSession "+token.getText().toString();
+        b.execute();
     }
 
-    static class BackgroundTask extends AsyncTask<String, Void, String>
+    @SuppressLint("StaticFieldLeak")
+    class BackgroundTask extends AsyncTask<String, Void, String>
     {
         Socket socket;
-        DataOutputStream dataOutputStream;
-        DataInputStream dataInputStream;
-        String serverIP, messageToServer, messageFromServer;
+        //DataOutputStream dataOutputStream;
+        //DataInputStream dataInputStream;
+        PrintWriter printWriter;
+
 
         @Override
         protected String doInBackground(String... params) {
-            serverIP = params[0];
-            messageToServer = params [1];
             try {
-                socket = new Socket(serverIP, 9700);
-                dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                dataOutputStream.writeUTF(messageToServer);
-                System.out.println("messageToServer: "+messageToServer);
-                dataOutputStream.close();
+                socket = new Socket(ip, 9700);
+                //dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                //dataOutputStream.writeUTF(messageToServer);
+                printWriter = new PrintWriter(socket.getOutputStream());
+                printWriter.write(messageToServer);
+                printWriter.flush();
+                printWriter.close();
                 socket.close();
+                System.out.println("messageToServer: "+messageToServer);
+                //dataOutputStream.flush();
+                //dataOutputStream.close();
             } catch (IOException e){
                 e.printStackTrace();
             }
